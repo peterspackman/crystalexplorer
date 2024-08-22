186bac3a17a93cb404def9eaa256daae89fc5e3e
Peter Spackman <peterspackman@fastmail.com>
2024-08-22 17:02:45 +0800

    Setup windows packaging again - mostly working but missing libc++ dependencies when using mingw compilers

--- cmake/BundleWindows.cmake
+++ cmake/BundleWindows.cmake
@@ -1,7 +1,7 @@
 set(CPACK_GENERATOR NSIS
     CACHE STRING "Generator for packaging")
 set(RUNTIME_DESTINATION "/")
-set(ICON_FILE ${PROJECT_SOURCE_DIR}/icons/crystalexplorer.ico)
+set(WINDOWS_ICON_FILE "${PROJECT_SOURCE_DIR}/icons/crystalexplorer.ico")
 set(OS_BUNDLE WIN32)
 # Windows NSIS installer stuff
 set(RESOURCES_DESTINATION "/")
@@ -12,8 +12,22 @@ set(CMAKE_INSTALL_SYSTEM_RUNTIME_DESTINATION "${RUNTIME_DESTINATION}")
 set(APPS "\${CMAKE_INSTALL_PREFIX}/${PROJECT_NAME}.exe")
 include(InstallRequiredSystemLibraries)
 set(CMAKE_INSTALL_UCRT_LIBRARIES TRUE)
-configure_file(
-    ${PROJECT_SOURCE_DIR}/scripts/setup.iss.in
-    ${PROJECT_BINARY_DIR}/setup.iss
+
+# Enable start menu shortcut creation
+set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+set(CPACK_NSIS_MODIFY_PATH ON)
+
+# Set the name for your shortcut
+set(CPACK_NSIS_DISPLAY_NAME "CrystalExplorer")
+
+# Create a start menu shortcut
+set(CPACK_NSIS_MENU_LINKS
+  "${PROJECT_NAME}.exe" "CrystalExplorer"
 )
-find_program(WINDEPLOYQT_COMMAND NAMES windeployqt HINTS "${CMAKE_PREFIX_PATH}/bin")
+
+# Optional: Set an icon for the shortcut
+set(CPACK_NSIS_MUI_ICON "${WINDOWS_ICON_FILE}")
+set(CPACK_NSIS_MUI_UNIICON "${WINDOWS_ICON_FILE}")
+
+# Optional: Create a desktop shortcut
+set(CPACK_CREATE_DESKTOP_LINKS "CrystalExplorer")
