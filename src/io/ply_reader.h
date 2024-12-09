bdfee4d793641e65c14f592a259edd6bff70f899
Peter Spackman <peterspackman@fastmail.com>
2024-12-09 08:25:43 +0800

    Tidy up code formatting for ply_reader

--- src/io/ply_reader.h
+++ src/io/ply_reader.h
@@ -17,25 +17,20 @@ public:
   static std::unique_ptr<Mesh> loadFromFile(const QString &filepath);
 
 private:
-  // Make these virtual for testing if needed
   virtual void parseHeader();
   virtual void requestProperties();
   virtual void readFileData();
   virtual std::unique_ptr<Mesh> constructMesh();
 
-  // Helper methods for property processing
   void processVertexProperties();
   void setMeshProperty(Mesh *mesh, const QString &displayName,
                        const std::shared_ptr<tinyply::PlyData> &prop);
 
-  // Static helper for file reading
   static std::vector<uint8_t> readFileBinary(const QString &filepath);
 
-  // Member variables
   std::unique_ptr<std::istream> m_stream;
   std::unique_ptr<tinyply::PlyFile> m_plyFile;
 
-  // Property storage
   std::shared_ptr<tinyply::PlyData> m_vertices;
   std::shared_ptr<tinyply::PlyData> m_faces;
   std::shared_ptr<tinyply::PlyData> m_normals;
