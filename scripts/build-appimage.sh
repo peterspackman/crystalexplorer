0c84db8a7f68eecc284e8376acf12802038d74de
Peter Spackman <peterspackman@fastmail.com>
2025-02-14 14:53:36 +0800

    Add appimage build

---
+++ scripts/build-appimage.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+BUILD_DIR="build"
+ARCH="x86_64"
+NAME="linux-x86_64-static"
+
+# Download required tools
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+wget https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage
+chmod +x linuxdeploy*.AppImage
+
+cmake . -B"${BUILD_DIR}" -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_HOST_OPT=OFF -GNinja \
+    -DCMAKE_CXX_FLAGS="-O2 -static-libgcc -static-libstdc++" \
+    -DCMAKE_C_FLAGS="-O2 -static-libgcc" -DBUILD_DOCS=ON \
+    -DCPACK_SYSTEM_NAME="${NAME}" -DCMAKE_INSTALL_PREFIX="/usr"
+
+cmake --build "${BUILD_DIR}"
+cmake --install "${BUILD_DIR}" --prefix /usr -DDESTDIR=AppDir
+
+# Create AppImage
+export QMAKE="$(which qmake)"
+./linuxdeploy-x86_64.AppImage --appdir=AppDir \
+    --plugin qt \
+    --output appimage \
+    --icon-file=icons/crystalexplorer.png \
+    --desktop-file=resources/crystalexplorer.desktop
